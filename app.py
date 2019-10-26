43816cd01ef3ce4dca5e2297dacaf6332e8975fe
nnguyen74 <nnguyen11296@gmail.com>
2019-10-26 18:26:46 -0400

    Initialize files

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse
+import werkzeug
+import boto3
+from uuid import uuid4
+
+AWS_ACCESS_KEY_ID = 'AKIA243KINZ64ZOJSI4G'
+AWS_SECRET_ACCESS_KEY = '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'
+bucket = 'digital-museum'
+
+app = Flask(__name__)
+api = Api(app)
+
+def s3Upload(file):
+    try:
+        filename = file.filename
+        random_file_name = ''.join([str(uuid4().hex[:6]) + "-", filename])
+        s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        s3.upload_fileobj(file, bucket, random_file_name)
+        return {"data":filename}
+    except Exception as e:
+        print(e)
+        return {"error": e}
+
+
+
+class server(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+        args = parser.parse_args()
+        file = args['file']
+        return s3Upload(file)
+
+api.add_resource(server, "/server")
+
+if __name__ == '__main__':
+    app.run(debug=True)
